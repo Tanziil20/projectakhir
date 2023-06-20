a3d7cf04dbd168959a4e71a9bacf39117098efe4
Tanziil <100509407+Tanziil20@users.noreply.github.com>
2023-06-20 15:01:43 +0700

    update

---
+++ app/src/main/java/sns/example/projectakhir/DashboardPembeli.java
@@ -0,0 +1,12 @@
+package sns.example.projectakhir;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DashboardPembeli extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard_pembeli);
+    }
+}
